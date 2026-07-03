da28e3dfb2c9d6441256531bab5105d79e04e5c6
dajneem23 <dajneem23@users.noreply.github.com>
2026-07-03 02:12:38 +0000

    deploy: 9f5a21054035b2bb406b0710bdc25be0a5942461

---
+++ docs/development/stremio_core/types/streaming_server/response/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["SettingsOptionValue"],"struct":["ArchiveCreateResponse","GetHTTPSResponse","OpensubtitlesParamsResponse","SettingsOption","SettingsOptionSelection","SettingsResponse"]};
